db4053057678d2dfa77c60d0c6f5f939f45e0618
Chase Yoo <yhryu1030@gmail.com>
2020-04-17 01:13:55 -0500

    Day 19

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,111 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+const alphaNumber = document.querySelector('.alphaContainer input');
+
+let redEffectBoolean = false;
+let rgbSplitBoolean = false;
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => {
+            console.error(`Allow to use your camera!`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        
+        // Mess with them
+        if (redEffectBoolean) {
+            pixels = redEffect(pixels);
+        }
+        if (rgbSplitBoolean) {
+            pixels = rgbSplit(pixels);
+            ctx.globalAlpha = 0.7;
+        }
+        pixels = greenScreen(pixels);
+
+        // Put them back
+        ctx.putImageData(pixels, 0, 0);
+    }, 16); // get a frame every 16 milliseconds
+}
+
+function takePhoto() {
+    // play the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i = i + 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 70; // RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i = i + 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        pixels.data[i + 100] = pixels.data[i + 1]; // GREEN
+        pixels.data[i - 150] = pixels.data[i + 2]; // BLUE
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+            }
+    }
+
+    return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
